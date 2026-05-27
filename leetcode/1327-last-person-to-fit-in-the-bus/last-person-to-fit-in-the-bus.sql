6eb5cb869a163bc57e48bf09a4a8adc0ef48c4d5
HoangDM <105637714+hoang14102002@users.noreply.github.com>
2026-05-27 13:50:35 +0700

    Time: 898 ms (34.50%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ leetcode/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -2,7 +2,7 @@
 
     with temp as
     (
-        select *, sum(weight) over(order by turn) total_weight
+        select person_name, turn, sum(weight) over(order by turn) total_weight
         from queue
     )
     select person_name from temp
