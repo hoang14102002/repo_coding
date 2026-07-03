d8ca8d5a83afdb1a7af866686a6a8f7e5ade6ab2
HoangDM <105637714+hoang14102002@users.noreply.github.com>
2026-07-03 10:13:15 +0700

    Time: 575 ms (59.16%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/262-trips-and-users/trips-and-users.sql
+++ leetcode/262-trips-and-users/trips-and-users.sql
@@ -1,23 +1,14 @@
 # Write your MySQL query statement below
-with 
-banned_client_users as
+with filter_ban as 
 (
-    select * from users where role = 'client' and banned = 'yes'
-),
-banned_driver_users as
-(
-    select * from users where role = 'driver' and banned = 'yes'
+    select a.* from trips a 
+        left join users b on a.client_id = b.users_id
+        left join users c on a.driver_id = c.users_id
+    where b.banned = "No" and c.banned = "No"
 )
-,filter_band as 
-(
-    select a.* 
-    from trips a 
-        left join banned_client_users b on a.client_id = b.users_id
-        left join banned_driver_users c on a.driver_id = c.users_id
-    where b.users_id is null and c.users_id is null
-)
-select request_at as 'Day', 
-    round(sum(case when status like 'cancelled%' then 1 else 0 end)/ count(distinct id),2) as 'Cancellation Rate'
-from filter_band
-where request_at between '2013-10-01' and '2013-10-03'
+select request_at as "Day",
+    round(sum(case when status like "cancel%" then 1 else 0 end)/count(id),2) as  "Cancellation Rate"
+from filter_ban
+where request_at between "2013-10-01" and "2013-10-03"
 group by request_at
+order by request_at
