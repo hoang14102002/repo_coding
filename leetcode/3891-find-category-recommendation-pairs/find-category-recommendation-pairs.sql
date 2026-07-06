0becf22dc727d78d37dfdcaff5c9bf0a9ef91fc3
HoangDM <105637714+hoang14102002@users.noreply.github.com>
2026-07-06 14:14:03 +0700

    Time: 769 ms (20.42%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/3891-find-category-recommendation-pairs/find-category-recommendation-pairs.sql
+++ leetcode/3891-find-category-recommendation-pairs/find-category-recommendation-pairs.sql
@@ -1,21 +1,16 @@
 # Write your MySQL query statement below
-    with temp as
-    (
-        select distinct a.user_id, b.category
-        from productpurchases a 
-            left join productinfo b on a.product_id = b.product_id
-    ),
-    temp2 as 
-    (
-        select t1.user_id, t1.category as category1, t2.category as category2 
-        from temp t1
-            left join temp t2 on t1.user_id = t2.user_id
-    )
-    select category1, category2, count(distinct user_id) as customer_count
-    from temp2 
-    where category1 < category2 
-    group by category1, category2
-    having count(distinct user_id) >= 3
-    order by customer_count desc, category1, category2
-    
-    
+
+with temp as
+(
+    select t1.*, t2.category 
+    from productpurchases t1
+        left join productinfo t2 on t1.product_id = t2.product_id
+)
+select a.category as category1, b.category as category2 ,
+    count(distinct a.user_id) as customer_count
+from temp a
+    left join temp b on a.user_id = b.user_id  
+where a.category < b.category
+group by a.category, b.category
+having count(distinct a.user_id) >= 3
+order by count(distinct a.user_id) desc, a.category, b.category
